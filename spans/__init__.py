953a67976e05e2410561c7e2f14b417977e08f33
cassirer-openai <cassirer@openai.com>
2025-05-21 21:32:43 -0700

    Fix typos in module docstring

--- spans/__init__.py
+++ spans/__init__.py
@@ -2,12 +2,12 @@
 This module provides one dimensional continuous set support for python.
 
 Python has a wonderful set class, it does however not handle continuous sets
-between two endpoints. This module tries to mitigate that. The ranges' behavoir
+between two endpoints. This module tries to mitigate that. The ranges' behavior
 are modeled after PostgresSQL 9.2's range types. Deviating from PostgresSQL's
-behavoir is considered a bug.
+behavior is considered a bug.
 
 In addition to the range types there are range sets. A range set is can be viewed
-as a mutable list of ranges. A set enables discontinious chunks to be grouped
+as a mutable list of ranges. A set enables discontinuous chunks to be grouped
 together.
 """
 
